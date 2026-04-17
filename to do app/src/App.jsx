62bb23393074aadfdb3581d2d1d6e1dbf288867a
Tarun <tarunkrish9905@gmail.com>
2026-04-17 14:22:47 +0530

    Reactjs

---
+++ to do app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./component/Header"
+import Todoinput from "./component/Todoinput"
+
+function App() {
+  return (
+    <>
+  
+    <Header />
+     <Todoinput />
+    </>
+    
+  )
+}
+
+export default App
---
+++ to do app/src/component/Todoinput.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+function Todoinput({addTodo}) {
+    const [todo, setTodo] = useState("")
+
+    const handleChange = () => {
+        if (todo.trim() !== "") {
+            addTodo(todo)
+            setTodo("")
+        }   
+    }
+    return (
+        <div> 
+            <input type="text" placeholder="Enter your todo" value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <button onClick={handleChange}>Add Todo</button>
+        </div>
+        
+    )
+}
+
+export default Todoinput
+
